4b6fd8a870348fbc9aa580a7103c7047cde98ffa
ch-wannaporn <ch.wannaporn_st@tni.ac.th>
2022-01-16 18:23:10 +0700

    add SearchNumber class

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+class SearchNumber {
+    private $arr = [];
+    
+    function __construct()
+    {
+        for ($i = 1; $i<=100; $i++) {
+            $this->addNumber($i);
+        }
+    }
+
+    function addNumber($value) {
+        $this->arr[$value/10][$value%10] = $value;
+    }
+
+    function deleteNumber($value) {
+        $this->arr[$value/10][$value%10] = null;
+    }
+
+    function search($value) {
+        $ten_digit = $value/10;
+        $digit = $value%10;
+
+        if(array_key_exists($ten_digit, $this->arr) && 
+            array_key_exists($digit, $this->arr[$ten_digit]) && 
+            $this->arr[$ten_digit][$digit])
+            echo "Found";
+        else
+            echo "Not Found";
+    }
+}
+
+$num = $_GET['number'];
+
+$search = new SearchNumber();
+$search->search($num);
