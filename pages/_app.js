ed9efda21977e2997540984ef282368b2be8a7fe
ch-wannaporn <ch.wannaporn_st@tni.ac.th>
2022-01-17 12:45:33 +0700

    use context

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import {AppWrapper} from '../context/app'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AppWrapper>
+      <Component {...pageProps} />
+    </AppWrapper>
+  )
 }
 
 export default MyApp
