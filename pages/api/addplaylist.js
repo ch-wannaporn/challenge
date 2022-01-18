8a705b82be74c127d1d91f95156a7d027f70c534
ch-wannaporn <ch.wannaporn_st@tni.ac.th>
2022-01-18 09:36:07 +0700

    add db.end()

--- pages/api/addplaylist.js
+++ pages/api/addplaylist.js
@@ -2,7 +2,7 @@ import db from './db';
 
 const addplaylist = async (req, res) => {
     const results = await db.query('INSERT INTO playlists(id, name) VALUES (?, ?)', [req.query.id, req.query.name]);
-    await db.end()
+    await db.end();
     return res.json(results);
 }
 
